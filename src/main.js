8cb4173deced4edd471eff051d98f74679f110e5
asantosmanu <manuelasantos452006@gmail.com>
2023-08-04 15:08:43 -0300

    ok

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import './assets/main.css'
+//import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap'
 
-createApp(App).mount('#app')
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
