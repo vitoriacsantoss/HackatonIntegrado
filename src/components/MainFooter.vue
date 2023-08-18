2fddf7e5df9bce927bcd7a7439979f8642fa036a
asantosmanu <manuelasantos452006@gmail.com>
2023-08-18 15:14:35 -0300

    mainfooter

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,12 +1,13 @@
 <template>
-    <div class="top-footer">
+    <div class="main-footer">
+        <h1>CONTATO</h1>
         <div>
-            
+
             CRECI 12345
         </div>
         <div>
             <img class="img1" src="telefone.png" alt="">
-             (47) 1234-1234
+            (47) 1234-1234
         </div>
         <div>
             <img class="img" src="https://cdn.icon-icons.com/icons2/792/PNG/512/whatsapp_icon-icons.com_65542.png" alt="">
@@ -16,30 +17,36 @@
             <img class="img1" src="email.png" alt="">
             contato@contato.com.br
         </div>
-       
+
     </div>
 </template>
 
 <style scoped>
-    .top-footer {
-        padding: 10px 10px 500px 200px;
-        background-color: #02193c;
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-    }
-    
-    .top-footer div {
-        color: #AAAAAA;
-        margin: 0 15px 0 0 ;
-        font-size: 12px;
-    }
-    img{
-        height: 12px;
-        width: 12px;
-    }
-    .img1{
-      height: 20px;
-      width: 20px;
-    }
-</style>
+.main-footer {
+    padding: 10px 10px 500px 100px;
+    background-color: #02193c;
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+}
+
+.main-footer div {
+    color: #AAAAAA;
+    margin: 0 15px 0 0;
+    font-size: 12px;
+}
+
+img {
+    height: 12px;
+    width: 12px;
+}
+
+.img1 {
+    height: 20px;
+    width: 20px;
+}
+
+h1 {
+    font-size: medium;
+    color: white;
+}</style>
