1c809e1946aee5193d09df31c0f23a22819e28a4
Bruna Henz <brunaehenz@gmail.com>
2023-08-18 14:18:16 -0300

    Itens Footer

--- src/components/TopFooter.vue
+++ src/components/TopFooter.vue
@@ -7,7 +7,8 @@
            <li>ITEM 1</li>  
            <li>ITEM 2</li>  
            <li>ITEM 3</li>  
-           <li>ITEM 3</li>  
+           <li>ITEM 4</li>  
+           <li>ITEM 5</li>  
         </ul>
         
     </div>
