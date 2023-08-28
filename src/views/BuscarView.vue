a3d43549ef2b6a1f9562b55998a6f89e4eeb54df
Vi140906 <santosviih360@gmail.com>
2023-08-28 11:01:39 -0300

    buscar imovel concluido

--- src/views/BuscarView.vue
+++ src/views/BuscarView.vue
@@ -1,286 +1,329 @@
 <script setup>
-    import {  ref } from 'vue'
+import { ref } from 'vue'
 
-    const estado = ref('')
+const estado = ref('')
 
 </script>
 
 
 <template>
-
-
     <div class="banner">
         <div class="inner-text">
             <h1>BUSCAR ÍMOVEIS</h1>
         </div>
     </div>
     <div class="principal">
-    <div class="form">
-        <h2>BUSCA AVANÇADA</h2>
-    <label for="estado"></label>
-      <select class="caixinha" v-on:keypress="ok = false" v-model="estado">
-        <option value="Alugar">Alugar</option>
-        <option value="Comprar">Comprar</option>
-      </select>
-        <p>Tipo de imóvel</p>
-         <select class="caixinha" v-on:keypress="ok = false" v-model="estado">
-        <option value="Casa">Casa</option>
-        <option value="Apartamento">Apê</option>
-        <option value="Terreno">Terreno</option>
-        </select>
-        <p>Cidades</p>
-        <select class="caixinha" v-on:keypress="ok = false" v-model="estado">
-        <option value="Joinville">Joinville</option>
-        <option value="Araquari">Araquari</option>
-      </select>
-        <p>Bairros</p>
-        <select class="caixinha" v-on:keypress="ok = false" v-model="estado">
-        <option value="Joinville">Itinga</option>
-        <option value="Araquari">Araquari</option>
-      </select>
-      <div class="lado">
-      <p>Quartos</p>
-        <select class="caixinha" v-on:keypress="ok = false" v-model="estado">
-        <option value="um">01</option>
-        <option value="dois">02</option>
-        <option value="mais">mais</option>
-      </select>
-      <p>Suítes</p>
-      <select class="caixinha" v-on:keypress="ok = false" v-model="estado">
-        <option value="um">01</option>
-        <option value="dois">02</option>
-        <option value="mais">mais</option>
-      </select>
-      <p>Banheiros</p>
-      <select class="caixinha" v-on:keypress="ok = false" v-model="estado">
-        <option value="um">01</option>
-        <option value="dois">02</option>
-        <option value="mais">mais</option>
-      </select>
-      <p>Garagem</p>
-      <select class="caixinha" v-on:keypress="ok = false" v-model="estado">
-        <option value="um">01</option>
-        <option value="dois">02</option>
-        <option value="mais">mais</option>
-      </select>
-    </div>
-    </div>
-
-
-    <div class="containerimg">
-    <div class="oi">
-        <img class="img1" src="APT.jpg" alt="">
-    </div>
-    <div>
-        <img class="img2" src="1.png" alt="">
-    </div>
-    <div>
-        <img class="img3" src="APT2.jpg" alt="">
-    </div>
-    </div>
-
-
-
-
-    <div class="containerinfo">
-        <div class="info">
-            <div class="bairro">Floresta | Joinville</div>
-            <div class="rs">R$390.000,00</div>
-            <div class="m2">50,90m²</div>
+        <div class="form">
+            <h2>BUSCA AVANÇADA</h2>
+            <label for="estado"></label>
+            <select class="caixinha" v-on:keypress="ok = false" v-model="estado">
+                <option value="Alugar">Alugar</option>
+                <option value="Comprar">Comprar</option>
+            </select>
+            <p>Tipo de imóvel</p>
+            <select class="caixinha" v-on:keypress="ok = false" v-model="estado">
+                <option value="Casa">Casa</option>
+                <option value="Apartamento">Apê</option>
+                <option value="Terreno">Terreno</option>
+            </select>
+            <p>Cidades</p>
+            <select class="caixinha" v-on:keypress="ok = false" v-model="estado">
+                <option value="Joinville">Joinville</option>
+                <option value="Araquari">Araquari</option>
+            </select>
+            <p>Bairros</p>
+            <select class="caixinha" v-on:keypress="ok = false" v-model="estado">
+                <option value="Joinville">Itinga</option>
+                <option value="Araquari">Araquari</option>
+            </select>
+            <div class="lado">
+                <p>Quartos</p>
+                <select class="caixinha" v-on:keypress="ok = false" v-model="estado">
+                    <option value="um">01</option>
+                    <option value="dois">02</option>
+                    <option value="mais">mais</option>
+                </select>
+                <p>Suítes</p>
+                <select class="caixinha" v-on:keypress="ok = false" v-model="estado">
+                    <option value="um">01</option>
+                    <option value="dois">02</option>
+                    <option value="mais">mais</option>
+                </select>
+                <p>Banheiros</p>
+                <select class="caixinha" v-on:keypress="ok = false" v-model="estado">
+                    <option value="um">01</option>
+                    <option value="dois">02</option>
+                    <option value="mais">mais</option>
+                </select>
+                <p>Garagem</p>
+                <select class="caixinha" v-on:keypress="ok = false" v-model="estado">
+                    <option value="um">01</option>
+                    <option value="dois">02</option>
+                    <option value="mais">mais</option>
+                </select>
+            </div>
         </div>
-        <div class="info1">
-            <div class="bairro">Bucarein | Joinville</div>
-            <div class="rs">R$340.000,00</div>
-            <div class="m2">68,88m²</div>
+
+
+        <div class="containerimg">
+            <div class="oi">
+                <img class="img1" src="APT.jpg" alt="">
+            </div>
+            <div>
+                <img class="img2" src="1.png" alt="">
+            </div>
+            <div>
+                <img class="img3" src="APT2.jpg" alt="">
+            </div>
+            <div>
+                <img class="img4" src="AP3.jpg" alt="">
+            </div>
+
         </div>
-        <div class="info2">
-            <div class="bairro">Profipo | Joinville</div>
-            <div class="rs">R$320.000,00</div>
-            <div class="m2">76,00m²</div>
+
+
+
+
+        <div class="containerinfo">
+            <div class="info">
+                <div class="bairro1">Floresta | Joinville</div>
+                <div class="rs">R$390.000,00</div>
+
+                <div>
+                    <img class="m2" src="m2.png" alt=""> 50,90m²
+                </div>
+                <div>
+                    <img class="quarto" src="quarto.png" alt="">
+                    <div class="qrt">
+                        2 Quartos
+                    </div>
+                </div>
+                <div>
+                    <img class="banheiro" src="banheiro.png" alt="">
+                    1 Banheiro
+                </div>
+            </div>
+
+
+            <div class="info1">
+                <div class="bairro1">Bucarein | Joinville</div>
+                <div class="rs">R$340.000,00</div>
+                <div>
+                    <img class="m2" src="m2.png" alt="">
+                    68,88m²
+                </div>
+                <div>
+                    <img class="quarto" src="quarto.png" alt="">
+                    <div class="qrt">
+                        2 Quartos
+                    </div>
+
+                </div>
+                <div>
+                    <img class="banheiro" src="banheiro.png" alt="">
+                    1 Banheiro
+                </div>
+            </div>
+            <div class="info2">
+                <div class="bairro1">Profipo | Joinville</div>
+                <div class="rs">R$320.000,00</div>
+                <div>
+                    <img class="m2" src="m2.png" alt="">
+                    76,00m²
+                </div>
+                <div>
+                    <img class="quarto" src="quarto.png" alt="">
+                    <div class="qrt">
+                        2 Quartos
+                    </div>
+
+                </div>
+                <div>
+                    <img class="banheiro" src="banheiro.png" alt="">
+                    1 Banheiro
+                </div>
+            </div>
         </div>
     </div>
-</div>
-
-
-    <div class="containerimg2">
-    <div class="oii">
-        <img class="img4" src="APT.jpg" alt="">
-    </div>
-    <div>
-        <img class="img5" src="1.png" alt="">
-    </div>
-    <div>
-        <img class="img6" src="APT2.jpg" alt="">
-    </div>
-    </div>
-
-
-    <div class="containerinfo2">
-        <div class="info3">
-            <div class="bairro">Floresta | Joinville</div>
-            <div class="rs">R$390.000,00</div>
-            <div class="m2">50,90m²</div>
+    <div class="info3">
+        <div class="bairro">Fátima | Joinville</div>
+        <div class="rs">R$320.000,00</div>
+        <div>
+            <img class="m2" src="m2.png" alt="">
+            76,00m²
         </div>
-        <div class="info4">
-            <div class="bairro">Bucarein | Joinville</div>
-            <div class="rs">R$340.000,00</div>
-            <div class="m2">68,88m²</div>
+        <div>
+            <img class="quarto" src="quarto.png" alt="">
+            <div class="qrt">
+                2 Quartos
+            </div>
+
         </div>
-        <div class="info5">
-            <div class="bairro">Profipo | Joinville</div>
-            <div class="rs">R$320.000,00</div>
-            <div class="m2">76,00m²</div>
+        <div>
+            <img class="banheiro" src="banheiro.png" alt="">
+            1 Banheiro
         </div>
     </div>
-
-
-
-
 </template>
 
 
 <style scoped>
-.info3{
-    margin-top: 60%;
-    margin-left: 10%;
-    font-size: large;
+.qrt {
+    margin-left: 50px;
+    margin-top: -15%;
+
 }
-.info4{
-    margin-top: 45%;
-    margin-left: 10%;
-    font-size: large;
-    margin-bottom: 20% ;
+
+.m2 {
+    color: #f28a31;
+    margin-right: 10px;
+    text-align: center;
+    font-size: 10px;
+    font-weight: 40;
+    width: 40px;
+    height: 40px;
+    margin-left: 60%;
 }
-.info5{
-    margin-top: 15%;
-    margin-left: 10%;
-    font-size: large;
-    margin-bottom: 30%;
+
+.quarto {
+    color: #f28a31;
+    margin-right: 10px;
+    margin-top: -17%;
+    text-align: center;
+    font-size: 10px;
+    font-weight: 40;
+    width: 40px;
+    height: 40px;
+
 }
-.img4{
-    height: 300px;
-    width: 300px;
-    margin-bottom: 20%;
-    margin-left: 70px;
-    margin-top: 05px;
-}
-.img5{
-    height: 300px;
-    width: 300px;
-    margin-bottom: 20%;
-    margin-left: 70px;
-    margin-top: -110px;
-}
-.img6{
-    height: 300px;
-    width: 300px;
-    margin-bottom: 20%;
-    margin-left: 70px;
-    margin-top: -150px;
+
+.banheiro {
+    color: #f28a31;
+    margin-right: 10px;
+    text-align: center;
+    font-size: 10px;
+    font-weight: 40;
+    width: 40px;
+    height: 40px;
 }
 
 
-.containerinfo2{
+.containerinfo {
     display: flex;
     flex-direction: column;
-    margin-left: 61%;
-    margin-top: -100%;
+    margin-left: 2%;
 }
-.containerimg2{
-    margin-left: 38%;
-    display: flex;
-    flex-direction: column;
-}
-.rs{
-    margin-top: 2px;
-    margin-bottom: 8px;
-}
-.bairro{
+
+.bairro1 {
+
     font-size: 40px;
     font-weight: bold;
-    color:#f28a31;
+    color: #f28a31;
 }
-.info{
-    margin-top: 20%;
+
+.bairro {
+margin-top: 20%;
+    font-size: 40px;
+    font-weight: bold;
+    color: #f28a31;
+}
+
+.info {
+    margin-top: 18%;
+    margin-left: 10%;
+    font-size: medium;
+    width: 400px;
+
+}
+
+.info1 {
+    margin-top: 48%;
     margin-left: 10%;
     font-size: large;
+    width: 400px;
 }
-.info1{
-    margin-top: 60%;
+
+.info2 {
+    margin-top: 50%;
     margin-left: 10%;
     font-size: large;
+    width: 400px;
 }
-.info2{
-    margin-top: 60%;
-    margin-left: 10%;
+
+.info3 {
+    margin-top: -35%;
+    margin-left: 61%;
     font-size: large;
+    width: 400px;
+    margin-bottom: 8%;
 }
-.containerinfo{
-    display: flex;
-    flex-direction: column;
-}
-.containerimg{
-    display: flex;
-    flex-direction: column;
-}
-.img1{
+
+.img1 {
     height: 300px;
     width: 300px;
     margin-bottom: 20%;
     margin-left: 70px;
     margin-top: 50px;
 }
-.img2{
+
+.img2 {
     height: 300px;
     width: 300px;
     margin-bottom: 20%;
     margin-left: 70px;
 }
-.img3{
+
+.img3 {
     height: 300px;
     width: 300px;
     margin-bottom: 20%;
     margin-left: 70px;
 }
-.principal{
+
+.img4 {
+    height: 300px;
+    width: 300px;
+    margin-bottom: 80%;
+    margin-left: 70px;
+}
+
+.rs {
+    margin-top: 2px;
+    margin-bottom: 8px;
+}
+
+.containerimg {
+    display: flex;
+    flex-direction: column;
+    
+}
+
+.principal {
     display: flex;
     flex-direction: row;
 }
-.informacoes{
-    display: flex;
-    align-items: flex-start;
-    flex-direction: column;
-    padding-left: 250px;
-    color: black;
-}
-.img{
-    display: flex;
-    align-items: flex-start;
-    flex-direction: column;
-    padding-left: 250px;
-    color: black;
-}
-p{
+
+p {
     margin-top: 05%;
     color: #ffffff;
     font-weight: bold;
 }
-.caixinha{
+
+.caixinha {
     border-radius: 5px;
     padding: 5px 130px;
     background-color: #ffffff;
     border-color: #ffffff;
 }
-input{
+
+input {
     border-radius: 5px;
     padding: 5px 0px;
     background-color: #ffffff;
     border-color: #ffffff;
 }
-.inputUm{
-    margin-top: 10px;
-}
-.form{
+
+.form {
     border-radius: 2%;
     margin-left: 200px;
     margin-top: 50px;
@@ -289,12 +332,17 @@ input{
     width: 25%;
     background-color: #f28a31;
 }
-h2{
+
+h2 {
     color: white;
-    font-size:large ;
+    font-size: large;
 }
-.banner{
-    background: url(https://www.rafaelfonsecaimoveis.com.br/assets/img/anuncie/banner-anuncie.png) top left repeat; background-repeat: no-repeat; background-size: cover;background-size: cover;
+
+.banner {
+    background: url(https://www.rafaelfonsecaimoveis.com.br/assets/img/anuncie/banner-anuncie.png) top left repeat;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-size: cover;
     height: 280px;
     background-position: center center;
     background-repeat: no-repeat;
